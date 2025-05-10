a11c82533d8f7b4981539dd83c62684ec7dc3735
ennio <ennio.nick@gmail.com>
2025-05-10 13:30:26 +0200

    css links added

--- src/main/main.jsx
+++ src/main/main.jsx
@@ -2,6 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import '../../index.css'
 import '../components/Header/header.css'
+import '../components/Footer/footer.css'
+import '../components/DefaultMain/default.css'
+import '../components/Main/main.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
