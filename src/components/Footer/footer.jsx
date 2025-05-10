64671a77222138e8216a6f8742294fb5a10d59fc
ennio <ennio.nick@gmail.com>
2025-05-10 13:38:55 +0200

    Refactor footer and relocate button styles globally.
    Moved the options button styles from the Header CSS to a global stylesheet for reuse. Updated the Footer component to include a button with an icon, adding a new `add_icon.png` asset. Simplified and aligned the Footer's structure with better styling.

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -2,9 +2,10 @@
 function Footer() {
 
     return (
-        <div>
-            <h3>Footer</h3>
-        </div>
+
+        <footer className="footer">
+            <button className="optionsButton"><img src="/add_icon.png" className="icon" alt="Add enty"/></button>
+        </footer>
     )
 }
 
