6e8b70fe052b6b39cd673ebd07f6e5497560f30f
ennio <ennio.nick@gmail.com>
2025-05-10 13:30:42 +0200

    basic default main structure and css added

--- src/components/DefaultMain/default.jsx
+++ src/components/DefaultMain/default.jsx
@@ -3,8 +3,11 @@ function DefaultMain() {
 
     return (
 
-        <div>
-            <h3>Default</h3>
+        <div className="defaultMainContainer">
+            <img className="logo" src="/page_icon.png" alt="logo"/>
+            <h2>Start journaling</h2>
+            <p>Create your personal journal</p>
+            <p>Add one entry by clicking on the plus icon</p>
         </div>
     )
 }
