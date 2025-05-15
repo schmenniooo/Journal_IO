2cbc42bc5f19c3d89b3dea23d7eb5c35fd60a7d9
Schmidt, Ennio <Ennio.Schmidt@vector.com>
2025-05-15 14:49:18 +0200

    wrapper object unnecesary

--- src/model/journalEntryCreator.js
+++
@@ -1,9 +0,0 @@
-import JournalEntry from "./journalEntry.js";
-
-class JournalEntryCreator {
-
-    createJournalEntry(title, description) {
-
-        return new JournalEntry(title, description);
-    }
-}
