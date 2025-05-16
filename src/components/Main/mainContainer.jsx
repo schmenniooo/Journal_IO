edf548dbcb6133f5ac8f623fa8e97f464fd61d27
Schmidt, Ennio <Ennio.Schmidt@vector.com>
2025-05-16 12:52:45 +0200

    examples removed

--- src/components/Main/mainContainer.jsx
+++ src/components/Main/mainContainer.jsx
@@ -7,20 +7,7 @@ function MainContainer() {
 
         <main className='main-container'>
             <ul className="journalList">
-                <li className="journalListItem">
-                    <section className="journalEntry">
-                        <h4 className="journalHeading">Title</h4>
-                        <p className="journalContent">Content</p>
-                        <label className="journalDate">Created</label>
-                    </section>
-                </li>
-                <li className="journalListItem">
-                    <section className="journalEntry">
-                        <h4 className="journalHeading">Title</h4>
-                        <p className="journalText">Content</p>
-                        <label className="journalDate">Created</label>
-                    </section>
-                </li>
+
             </ul>
         </main>
     )
