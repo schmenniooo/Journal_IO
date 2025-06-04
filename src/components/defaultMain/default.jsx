d5131f4e5b3cfb06467727b7dfbc334ea730f64c
ennio <ennio.nick@gmail.com>
2025-06-04 13:47:08 +0200

    default main container design updated

--- src/components/defaultMain/default.jsx
+++ src/components/defaultMain/default.jsx
@@ -7,9 +7,10 @@ function DefaultMain() {
 
         <div className="defaultMainContainer">
             <img className="logo" src="/page_icon.png" alt="logo"/>
+            <div className="divider"></div>
             <h2>Start journaling</h2>
             <p>Create your personal journal</p>
-            <p>Add one entry by clicking on the plus icon</p>
+            <p>Add one entry by clicking on the plus icon..</p>
         </div>
     )
 }
