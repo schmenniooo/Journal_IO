c68f5a7f866213468cdfef00f07538895384dcea
ennio <ennio.nick@gmail.com>
2025-06-05 12:37:56 +0200

    edit entries design

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -11,7 +11,6 @@ function Footer({ onSaveEntry }) {
     const openEditModal = () => {
         setIsModalOpen(!isModalOpen);
     }
-
     const handleSave = (entry) => {
         if (onSaveEntry) {
             onSaveEntry(entry);
--- src/components/main/mainContainer.jsx
+++ src/components/main/mainContainer.jsx
@@ -10,14 +10,19 @@ function MainContainer({ entries = [], onDelete }) {
                 {entries.map(entry => (
                     <li key={entry.id} className="journalListItem">
                         <div className="journalEntry">
+                            <button className="journalActionButton" id="delete" onClick={(e) => {
+                                e.stopPropagation();
+                                onDelete(entry);
+                            }}>
+                                <img className="journalActionIcon" id="delete"src="/close_icon.png" alt="close"/>
+                            </button>
                             <h3 className="journalHeading">{entry.title}</h3>
                             <p className="journalText">{entry.content}</p>
-                            <button className="deleteButton" onClick={(e) => {
-                                    e.stopPropagation();
-                                    onDelete(entry);
-                                }}
-                            >
-                                <img className="deleteIcon" src="/close_icon.png" alt="close"/>
+                            <button className="journalActionButton" id="edit" onClick={(e) => {
+                                e.stopPropagation();
+                                // TODO: editEntry
+                            }}>
+                                <img className="journalActionIcon" id="edit" src="/edit_icon.png" alt="edit"/>
                             </button>
                             <div className="journalDate">{entry.date}</div>
                         </div>
