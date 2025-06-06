5351d6dc470798ecfad36e1a0809a5bce1ffe58b
ennio <ennio.nick@gmail.com>
2025-06-06 15:35:36 +0200

    basic structure

--- src/components/subComponents/searchView/searchView.jsx
+++ src/components/subComponents/searchView/searchView.jsx
@@ -3,10 +3,17 @@ import './searchView.css'
 
 function SearchView() {
 
+    const onSubmit = () => {
+
+        // TODO
+    }
+
     return (
 
-        <div>
-
+        <div className="searchContainer">
+            <form className="searchForm" onSubmit={onSubmit}>
+                <input className="searchField" placeholder="Type in title" type="text"/>
+            </form>
         </div>
     )
 }
