afdea8877cf623793d7f53ac60918f007df3c039
ennio <ennio.nick@gmail.com>
2025-06-08 15:36:15 +0200

    basic optionsView structure

--- src/components/subComponents/optionsView/optionsView.jsx
+++ src/components/subComponents/optionsView/optionsView.jsx
@@ -1,12 +1,28 @@
 
 import './optionsView.css'
 
-function OptionsView() {
+function OptionsView({ isOpen, onClose }) {
+
+    // TODO: Sort by, Streak etc.
+
+    const onCloseClick = (e) => {
+        e.preventDefault();
+        onClose();
+    }
+
+    if (!isOpen) return null;
 
     return (
 
-        <div>
-
+        <div className="modalOverlay">
+            <div className="optionsModal">
+                <ul className="optionsList">
+                    <li className="optionsListItem">
+                        <a className="optionsListItemAction" href="https://github.com/schmenniooo/Journal_IO">Documentation</a>
+                    </li>
+                </ul>
+            </div>
+            <button className="closeButton" onClick={onCloseClick}>×</button>
         </div>
     )
 }
