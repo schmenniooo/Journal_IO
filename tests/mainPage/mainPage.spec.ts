dc182f5696b8ada1c54e06be83e4c859777dedc3
ennio <ennio.nick@gmail.com>
2025-06-09 13:32:55 +0200

    example test added

--- tests/mainPage/mainPage.spec.ts
+++ tests/mainPage/mainPage.spec.ts
@@ -3,7 +3,7 @@ import { test, expect} from "@playwright/test";
 
 test.describe("mainPage", async () => {
 
-    test("Loading DefaultMain", async ({page}) => {
+    test("MainPage has Title", async ({page}) => {
 
         await page.goto("http://localhost:3000/");
         await expect(page).toHaveTitle("JournalIO");
