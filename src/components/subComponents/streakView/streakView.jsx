1a5cc27a4f9af36bd7bc49609814692bd4c06ed2
ennio <ennio.nick@gmail.com>
2025-06-15 18:21:51 +0200

    streak view added

---
+++ src/components/subComponents/streakView/streakView.jsx
@@ -0,0 +1,14 @@
+
+import './streakView.css'
+import DefaultModal from "../model/defaultModal.jsx";
+
+function StreakView() {
+
+    return (
+        <DefaultModal title="Streak View" onClose={}>
+
+        </DefaultModal>
+    )
+}
+
+export default StreakView;
