ca76dbf1db0db9b5e8de5033a2848f631ff70770
ennio <ennio.nick@gmail.com>
2025-06-23 09:03:52 +0200

    useEffect for setting streak text

--- src/components/subComponents/streakView/streakView.jsx
+++ src/components/subComponents/streakView/streakView.jsx
@@ -1,18 +1,26 @@
 
 import './streakView.css'
 import DefaultModal from "../model/defaultModal.jsx";
+import {useEffect, useState} from "react";
 
 function StreakView({isOpen, onClose, streak}) {
 
-    if (!isOpen) return null;
+    const [streakText, setStreakText] = useState("");
 
     // TODO: Appropriate Text for streak value
+    useEffect(() => {
+
+        // TODO
+
+    }, [streak]);
+
+    if (!isOpen) return null;
 
     return (
         <DefaultModal title="Streak View" onClose={onClose}>
             <div className="streakContainer">
                 <span className="streakValue">{streak}</span>
-                <p className="streakText"></p>
+                <p className="streakText">{streakText}</p>
             </div>
         </DefaultModal>
     )
