6692837e25b7436bcd842c71e770f679f9dc9b8c
ennio <ennio.nick@gmail.com>
2025-06-24 08:18:24 +0200

    some fixes

--- tests/visual/mainPage.spec.ts
+++ tests/visual/mainPage.spec.ts
@@ -1,9 +1,9 @@
 
 import { test, expect} from "@playwright/test";
 
-test.describe("mainPage", async () => {
+test.describe("visual", async () => {
 
-    test("MainPage has Title", async ({page}) => {
+    test("Title", async ({page}) => {
 
         await page.goto("/");
         await expect(page).toHaveTitle("JournalIO");
