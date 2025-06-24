2119fea3fd2cb24516f0090876c6e4a433a46bdd
ennio <ennio.nick@gmail.com>
2025-06-24 08:42:10 +0200

    streak view title

--- src/components/subComponents/streakView/streakView.jsx
+++ src/components/subComponents/streakView/streakView.jsx
@@ -25,7 +25,7 @@ function StreakView({isOpen, onClose, streak}) {
     if (!isOpen) return null;
 
     return (
-        <DefaultModal title="Streak View" onClose={onClose}>
+        <DefaultModal title="Your Streak" onClose={onClose}>
             <div className="streakContainer">
                 <img className="streakFlame" src="/icons/streak_icon.png" alt="streakFlame"/>
                 <span className="streakValue">{streak}</span>
