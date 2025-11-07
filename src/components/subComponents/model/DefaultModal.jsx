67c89730b889483bbf30dc78bd2d8f87747dafe3
Schmidt, Ennio <Ennio.Schmidt@vector.com>
2025-11-07 13:36:54 +0100

    ux -> backdrop click for all modals

--- src/components/subComponents/model/DefaultModal.jsx
+++ src/components/subComponents/model/DefaultModal.jsx
@@ -3,8 +3,14 @@ import './DefaultModal.css'
 
 function DefaultModal({title, children, onClose}) {
 
+    const handleBackdropClick = (e) => {
+        if (e.target === e.currentTarget) {
+            onClose();
+        }
+    }
+
     return (
-        <div className="modalOverlay">
+        <div className="modalOverlay" onClick={handleBackdropClick}>
             <div className="modal">
                 <header className="modalHeader">
                     <h2 className="modalTitle">{title}</h2>
