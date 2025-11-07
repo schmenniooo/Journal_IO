eca88f186fc8fd642d433f321ed06ecd570c6bb4
Schmidt, Ennio <Ennio.Schmidt@vector.com>
2025-11-07 14:06:06 +0100

    main.tf

---
+++ main.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.99.1"
+    }
+    awscc = {
+      source  = "hashicorp/awscc"
+      version = "1.43.0"
+    }
+  }
+}
+
+provider "aws" {
+  // secrets will be set by the github workflow of a specific IAM-user
+  // To plan config locally configure aws cli with the appropriate credentials
+  region = "eu-central-1"
+}
+
+resource "aws_amplify_app" "Journal_IO_App" {
+  name = ""
+}
+
+resource "aws_amplify_branch" "main" {
+
+}
+
+resource "aws_amplify_domain_association" "app_domain" {
+  app_id      = aws_amplify_app.Journal_IO_App.id
+  domain_name = aws_route53_zone.domain.name
+}
+
+resource "aws_route53_zone" "domain" {
+  name = "journal.io"
+}
