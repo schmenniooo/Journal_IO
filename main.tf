b334188fd8f21fc2b10882e4c5ba1e7f406069cb
schmenniooo <166793900+schmenniooo@users.noreply.github.com>
2025-11-08 14:20:22 +0100

    JIO-Terraform (#41)
    * main.tf
    
    * variables.tf file added
    
    * some fixes
    
    * some fixes adn env added to workflow
    
    * using other resource
    
    * safe
    
    * using hosted zone
    
    * gitignore
    
    * gitignore
    
    * safe
    
    * setting credentials in tf config
    
    * up and downloading tf artifact
    
    * deprecated version
    
    * soem fixes
    
    * auto approve removed
    
    * tf run command removed
    
    ---------
    
    Co-authored-by: Schmidt, Ennio <Ennio.Schmidt@vector.com>

---
+++ main.tf
@@ -0,0 +1,43 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.99.1"
+    }
+  }
+}
+
+provider "aws" {
+  // secrets will be set by the github workflow of a specific IAM-user
+  // To plan config locally configure aws cli with the appropriate credentials
+  region     = "eu-central-1"
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+
+resource "aws_amplify_app" "Journal_IO_App" {
+  name = "journal_io_app"
+  repository = "https://github.com/schmenniooo/Journal_IO"
+  oauth_token = var.gh_token
+}
+
+resource "aws_amplify_branch" "main_branch" {
+  app_id = aws_amplify_app.Journal_IO_App.id
+  branch_name = "main"
+}
+
+resource "aws_amplify_domain_association" "app_domain" {
+  app_id      = aws_amplify_app.Journal_IO_App.id
+  domain_name = aws_route53_zone.domain.name
+
+  sub_domain {
+    branch_name = "main"
+    prefix      = "prod"
+  }
+}
+
+// TODO: Add domain registration
+
+resource "aws_route53_zone" "domain" {
+  name = "journal.io"
+}
